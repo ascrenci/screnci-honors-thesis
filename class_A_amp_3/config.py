2e17448462e2e9f876a7a65f7e8f1fcb11456e30
ascrenci <ascrenci@umass.edu>
2026-03-30 11:42:07 -0400

    updated branch in config file to correct branch

--- class_A_amp_3/config.py
+++ class_A_amp_3/config.py
@@ -1,5 +1,5 @@
 n_datapoints = 2000 # number of sims to run
-branch = "testing"
+branch = "master"
 asc_path = f"./{branch}/circuit_sim/class_A_amp.asc"
 temp_path = f"./{branch}/circuit_sim/temp_files/"
 data_file = f"{branch}/data_{n_datapoints}.npy"
